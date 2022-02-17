24ae2dd2ff2da2affee9a4970d12ccc58fe11322
Pranaykumar Kodam <45891116+pranay213@users.noreply.github.com>
2022-02-17 15:52:57 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-$conn=mysqli_connect('localhost','root','','music2') or die('error connection');  
+// $conn=mysqli_connect('localhost','root','','music2') or die('error connection');  
+$conn=mysqli_connect('remotemysql.com','M2nGoBaVzR','hlNNxLO1Ai','M2nGoBaVzR') or die('error connection'); 
 
 
 ?>
